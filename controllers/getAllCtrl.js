88ce3a8b8472d0b7e52ae51ff2413bf991c6379a
17eugene <drivercoua@gmail.com>
2021-12-16 04:09:30 +0200

    fix

--- controllers/getAllCtrl.js
+++ controllers/getAllCtrl.js
@@ -2,10 +2,9 @@ const db = require("../db");
 
 const getAllCtrl = async (req, res, next) => {
   try {
-    // const allHotdogs = await db.query(`SELECT * FROM hotdogs`);
+    const allHotdogs = await db.query(`SELECT * FROM hotdogs`);
 
-    res.send("Hello");
-    // res.status(200).json(allHotdogs.rows);
+    res.status(200).json(allHotdogs.rows);
   } catch (error) {
     next(error);
   }
